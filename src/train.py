71d18dce81e48dbae7a230f52329343ed7fc756d
saes13 <20071435@ce.pucmm.edu.do>
2025-04-28 15:54:40 -0400

    Actualizando estadisticas

--- src/train.py
+++ src/train.py
@@ -1,25 +1,20 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 import pickle
 from src.preprocessing import load_and_clean_data
 
-# Cargar y limpiar datos
 df = load_and_clean_data("data/BateadorVsPitcher.csv")
 print("Columnas después de limpieza: ", df.columns)
 
-# Definir características y objetivo
 X = df.drop(columns=["Embasado"])
 y = df["Embasado"]
 
-# Guardar columnas de características
 pickle.dump(X.columns, open("models/feature_columns.pkl", "wb"))
 
-# Dividir datos en entrenamiento y prueba
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entrenar modelo Random Forest con class_weight balanced
 model = RandomForestClassifier(
     n_estimators=100,
     criterion="gini",
@@ -32,13 +27,18 @@ model = RandomForestClassifier(
 )
 model.fit(X_train, y_train)
 
-# Realizar predicciones
 y_pred = model.predict(X_test)
 
-# Evaluar el modelo
 accuracy = accuracy_score(y_test, y_pred)
-print("Precisión del modelo: ", accuracy)
-print("\nReporte de Clasificación:\n", classification_report(y_test, y_pred))
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, zero_division=0)
+recall = recall_score(y_test, y_pred, zero_division=0)
+f1 = f1_score(y_test, y_pred, zero_division=0)
+
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1-Score: {f1:.4f}")
 
 # Guardar el modelo entrenado
 with open("models/model.pkl", "wb") as f:
